d519d97df0ea7886d6862ca22e201847c83c36d2
YUOEESF <121518927+YUOEESF@users.noreply.github.com>
2023-01-10 13:02:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const num1=document.querySelector("mm")
+const num2=document.querySelector("m")
+const num3=document.querySelector("qm")
+const num5=document.querySelector("as")
+const num6=document.querySelector("m1")
+const num7=document.querySelector("m2")
+const num8=document.querySelector("a3")
+
+
+
+
+
+
+
+
+
+
+function qsma(num1,num2){
+    return num1/num2;
+}
+
+function tarh(num1,num2){
+    return num1-num2;
+}
+
+function darb(num1,num2){
+    return num1*num2;
+}
+
+function sum(num1,num2){
+    return num1+num2;
+}
+
+
+
+function calculater (num1,num2,loay){
+    return loay (num1,num2);
+}
+console.log(tarh(6,4));
+console.log(calculater(20,6,sum))
